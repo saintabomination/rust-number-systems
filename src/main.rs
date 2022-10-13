33b220060574e16bcd2a21e079035d736f89a083
saintabomination <saint.abomination@protonmail.com>
2022-10-13 08:29:29 +0200

    Displaying the converted numbers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::io;
+use std::io::Write;
 
 fn main() {
+    print!("Enter a number: ");
+    io::stdout().flush().expect("Flush failed!");
+
     let mut scanned_number = String::new();
     io::stdin()
         .read_line(&mut scanned_number)
         .expect("Your input is not valid.");
 
     let number: u32 = scanned_number.trim().parse().expect("That is not a valid number!");
-    println!("{}", number);
+    println!("\nDEC: {}", number);
+    println!("HEX: {:X}", number);
+    println!("OCT: {:o}", number);
 }
